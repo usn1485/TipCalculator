e1aab6f64d1e0e37c1ce81c4c39d20ae3e59648d
Ujwala <contact.uju@gmail.com>
2019-06-29 18:44:45 -0500

    Changed few comments

--- TipCalculator.py
+++ TipCalculator.py
@@ -3,57 +3,60 @@ import math
 import os
 
 
-#Take user input for Bill amount.
-Bill=float(input("Enter the Bill Amount:"))
+#Take user input for bill amount.
+bill=float(input("Enter the bill Amount:"))
 
-# Write function to calculate Tip.
+# Write function to calculate tip.
 def tipCalculator(bill,percent):
     # convert the percent input to whole number if it is given as 0.15 
   if(percent<1):
     tipPercent=percent*100
-    Tip = bill*( tipPercent / 100 )                   
+    tip = bill*( tipPercent / 100 )                   
   else:
-    Tip = bill*( percent / 100 )
-  return Tip
+    tip = bill*( percent / 100 )
+  return tip
 
-# Verify if Bill amount is Positive else throw error
-if(Bill>0):
-     #Take user input for Tip Percent
-     percent=float(input("Enter the Tip Percent:"))
+# Verify if bill amount is Positive else throw error
+if(bill>0):
+     #Take user input for tip Percent
+     percent=float(input("Enter the tip Percent:"))
 
+     # Verify if Percentage is Positive else throw error
      if(percent>0):
-          Tip = tipCalculator(Bill,percent) 
 
-          #Check if user want to split the Tip amount?
+          # Calculate Tip for the given Bill Amount.
+          tip = tipCalculator(bill,percent) 
+
+          #Check if user want to split the tip amount.
           splitBool=(input("Do you want to split the tip? y/n?").strip().upper())
           
-          #Verify if user wants to split and then split the Tip amount.
+          #Verify if user wants to split and then split the tip amount.
           if splitBool=="Y":
-               # Take user input for no of people they want to split Tip between.
+               # Take user input for no of people they want to split tip between.
                noOfppl=int(input("Enter the No. of ppl you want to split the tip in:"))
 
                #If no of people are more than 1 then do the calculation to divide tip equally.
                if(noOfppl>1):                
-                    FinalTip= round(( Tip/noOfppl),2)
-                    print(f"Tip Split for {noOfppl} people is ${FinalTip}cents each.")
+                    FinalTip= round(( tip/noOfppl),2)
+                    print(f"tip Split for {noOfppl} people is ${FinalTip}cents each.")
                #If user changes mind and decides not to split. 
                elif (noOfppl==1):
-                    print(f"Tip for this table is ${round(Tip,2)}cents.")  
+                    print(f"tip for this table is ${round(tip,2)}cents.")  
                # if user enters 0 or something else.       
                else : 
-                    print("Tip should be paid by 1 or more people. Please check the input.") 
-          # If user doesn't want to split the Tip amount, calculate the Tip for per table          
+                    print("tip should be paid by 1 or more people. Please check the input.") 
+          # If user doesn't want to split the tip amount, calculate the tip for per table          
           elif splitBool=="N":
-               print(f"Tip for this table is ${round(Tip,2)}cents")
+               print(f"tip for this table is ${round(tip,2)}cents")
           #If user enters Other than Y/ N characters, print warning.      
           else: 
                print("Please Enter Y or N characters only.")
      #if user enters Negative or 0 Percent value, print warning  
      else:
        print("Percent must be positive")
-#If user enters the Bill amount 0 or negative, print warning       
+#If user enters the bill amount 0 or negative, print warning       
 else: 
-     print("The Bill amount must be greater than zero")
+     print("The bill amount must be greater than zero")
 
  
     
